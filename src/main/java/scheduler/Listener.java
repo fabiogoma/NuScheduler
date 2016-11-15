45a400032e8f917848f1162d8cc5d781df3c709a
Fabio G. Martins <fabiogoma@gmail.com>
2016-11-15 21:52:59 -0200

    implemented log4j

--- src/main/java/scheduler/Listener.java
+++ src/main/java/scheduler/Listener.java
@@ -1,6 +1,10 @@
 package scheduler;
 
+import java.text.ParseException;
 import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.json.JSONException;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
@@ -11,12 +15,13 @@ import com.amazonaws.services.sqs.model.Message;
 import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 
 public class Listener {
+	private static Logger logger = Logger.getLogger(Listener.class);
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, JSONException, ParseException {
 		AWSCredentials credentials = new ProfileCredentialsProvider().getCredentials();
 		AmazonSQS sqs = new AmazonSQSClient(credentials);
 		
-		System.out.println("Receiving messages from sqs_launch");
+		logger.info("Receiving messages from sqs_launch");
 		String myQueueUrl = "https://us-west-2.queue.amazonaws.com/678982507510/sqs_launch";
         ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(myQueueUrl);
         
@@ -25,7 +30,7 @@ public class Listener {
 	        for (Message message : messages) {
 	        	
 	        	Launcher launcher = new Launcher();
-	        	launcher.launchSpotInstance(message.getBody().toString());
+	        	launcher.scheduleLauncher(message.getBody().toString());
 	        	
 	        	String messageRecieptHandle = messages.get(0).getReceiptHandle();
 	        	sqs.deleteMessage(new DeleteMessageRequest(myQueueUrl, messageRecieptHandle));
